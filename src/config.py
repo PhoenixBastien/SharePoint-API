06e1449412d3635df0b5e3988b467560a6a0dd6e
Phoenix Bastien <91162508+PhoenixBastien@users.noreply.github.com>
2025-09-18 09:45:25 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,15 +1,18 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from office365.sharepoint.client_context import ClientCredential
 
-if load_dotenv():
-    CLIENT_ID = os.getenv('CLIENT_ID')
-    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-    TENANT_URL = os.getenv('TENANT_URL')
-    TEAM_SITE_URL = os.getenv('TEAM_SITE_URL')
-    EMAIL = os.getenv('EMAIL')
-    client_credentials = ClientCredential(CLIENT_ID, CLIENT_SECRET)
-else:
-    print('.env file not found')
-    quit()
+if not load_dotenv():
+    sys.exit('.env file not found')
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+TENANT = os.getenv('TENANT')
+ROOT_SITE_URL = os.getenv('ROOT_SITE_URL')
+ADMIN_SITE_URL = os.getenv('ADMIN_SITE_URL')
+TEST_SITE_URL = os.getenv('TEST_SITE_URL')
+EMAIL = os.getenv('EMAIL')
+
+client_credentials = ClientCredential(CLIENT_ID, CLIENT_SECRET)
